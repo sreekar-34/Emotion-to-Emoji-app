a836dfda441bee62532ddf552eceb926b9502ba1
sreekar-34 <95081023+sreekar-34@users.noreply.github.com>
2023-01-20 17:36:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,3 +32,41 @@ function speak(){
     var utterThis= new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+ if(error){
+    console.error(error);
+ }
+ else{
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction_1=results[0].label;
+    prediction_2=results[1].label;
+    speak();
+    if(results[0].label=='happy'){
+        document.getElementById("update_emoji").innerHTML="&#128512;"
+    }
+    if(results[0].label=='sad'){
+        document.getElementById("update_emoji").innerHTML="&#128546;"
+    }
+    if(results[0].label=='angry'){
+        document.getElementById("update_emoji").innerHTML="&#128545;"
+    }
+
+    if(results[1].label=='happy'){
+        document.getElementById("update_emoji2").innerHTML="&#128512;"
+    }
+    if(results[1].label=='sad'){
+        document.getElementById("update_emoji2").innerHTML="&#128546;"
+    }
+    if(results[1].label=='angry'){
+        document.getElementById("update_emoji2").innerHTML="&#128545;"
+    }
+ }
+}
